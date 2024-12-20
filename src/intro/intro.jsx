d1de7dff8c7758d9c5e25861e99078e1f5d5e2a1
Snir4game <sniramsalem1995@gmail.com>
2024-12-20 16:39:40 +0200

    add CSS styling for Intro component

--- src/intro/intro.jsx
+++ src/intro/intro.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react";
 import Homepage from "../home/homepage";
+import "./intro.css";
 const Intro = () => {
 
     const [showIntro, setShowIntro] = useState(true);
