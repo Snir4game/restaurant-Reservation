5f76277e93546e5376055dd38a9bf80240caef70
Snir4game <sniramsalem1995@gmail.com>
2024-12-20 16:54:40 +0200

    update script path in index.html, adjust fade animations duration in intro.css, and create main.jsx for rendering Intro component

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Intro from './src/intro/intro';
+import './src/intro/intro.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Intro />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
